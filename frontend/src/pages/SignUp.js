4239a12d2d753c9dfd09e6dcbc1609818737e77f
Biswarghya Biswas <iambiswarghya@gmail.com>
2023-02-10 21:59:28 +0530

    updated

--- frontend/src/pages/SignUp.js
+++ frontend/src/pages/SignUp.js
@@ -1,58 +1,82 @@
-import React, { useState } from "react";
+import React from "react";
+import "./SignUp.css";
+import { Formik, Form, Field } from "formik";
+import * as yup from "yup";
 
 const SignUp = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // add code to handle form submission here
-  };
+  const validationSchema = yup.object({
+    email: yup.string().email("Invalid email address").required("Email is required"),
+    password: yup.string().min(8, "Password must be at least 8 characters").required("Password is required"),
+    confirmPassword: yup
+      .string()
+      .oneOf([yup.ref("password"), null], "Passwords must match")
+      .required("Confirm Password is required"),
+  });
 
   return (
-    <div>
-      <h1>Sign Up</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="Enter Email"
-          required
-        /><br /><br />
+    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh", zIndex: "1000" }}>
+      <div style={{ backgroundColor: "white", padding: "30px", borderRadius: "10px" }}>
+        <h1 style={{ textAlign: "center", marginBottom: "43px", color: "#00A87E", fontSize: "32px", fontWeight: "800" }}>SIGN UP</h1>
+        <Formik
+          initialValues={{
+            email: "",
+            password: "",
+            confirmPassword: "",
+          }}
+          validationSchema={validationSchema}
+          onSubmit={(values) => {
+            // add code to handle form submission here
+          }}
+        >
+          {({ errors, touched }) => (
+            <Form>
+              <div style={{ marginBottom: "15px" }}>
+                <Field
+                  type="email"
+                  name="email"
+                  placeholder="Enter Email"
+                  style={{ width: "423px", padding: "10px", borderRadius: "10px", background: "rgba(217, 217, 217, 0.35)" }}
+                />
+                {errors.email && touched.email ? (
+                  <div style={{ color: "red" }}>{errors.email}</div>
+                ) : null}
+              </div>
 
-        <input
-          type="password"
-          id="password"
-          name="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="Password"
-          required
-        /><br /><br />
+              <div style={{ marginBottom: "15px" }}>
+                <Field
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  style={{ width: "423px", padding: "10px", borderRadius: "10px", background: "rgba(217, 217, 217, 0.35)" }}
+                />
+                {errors.password && touched.password ? (
+                  <div style={{ color: "red" }}>{errors.password}</div>
+                ) : null}
+              </div>
 
-        <input
-          type="password"
-          id="confirmPassword"
-          name="confirmPassword"
-          value={confirmPassword}
-          onChange={(e) => setConfirmPassword(e.target.value)}
-          placeholder="Confirm Password"
-          required
-        /><br /><br />
+              <div style={{ marginBottom: "15px" }}>
+                <Field
+                  type="password"
+                  name="confirmPassword"
+                  placeholder="Confirm Password"
+                  style={{ width: "423px", padding: "10px", borderRadius: "10px", background: "rgba(217, 217, 217, 0.35)" }}
+                />
+                {errors.confirmPassword && touched.confirmPassword ? (
+                  <div style={{ color: "red" }}>{errors.confirmPassword}</div>
+                ) : null}
+              </div>
 
-        <button type="submit">Sign Up</button>
-      </form>
-      <hr />
-      <h3>Sign up with:</h3>
-      <a href="#">Sign up with Google</a><br />
-      <a href="#">Sign up with Facebook</a><br />
-      <a href="#">Sign up with Twitter</a>
-    </div>
-  );
+              <button type="submit" style={{ width: "423px", marginTop: "30px", padding: "10px", background: "#00A87E", borderRadius: "10px", cursor: "pointer" }}>
+                Sign Up
+              </button>
+            </Form>
+          )}
+        </Formik>
+        <div style={{ display: "flex"}}>
+          <h1 style={{ textAlign: "center", marginTop: "35px", color: "#A5A5A5", fontSize: "16px", fontWeight: "400" }}>Or continue with</h1>
+          <h1 style={{ textAlign: "center", marginTop: "35px", color: "#A5A5A5", fontSize: "16px", fontWeight: "400" }}>Or continue with</h1>
+        </div>
+      </div>
+    </div> );
 };
-
 export default SignUp;
